4d37289a22e71ff8e9ccd579fa5d7ed5026ff738
ajuhvala2 <94096372+ajuhvala2@users.noreply.github.com>
2023-07-24 21:02:42 -0400

    Update wp-config.php

--- database-config/wp-config.php
+++ database-config/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'admin' );
 
 /** Database hostname */
-define( 'DB_HOST', '34.121.191.245' );
+define( 'DB_HOST', '35.239.219.98' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
